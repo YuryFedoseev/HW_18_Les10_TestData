5483ee2aa20e3d4fc52b6df99bff62dfbb5845ad
Yury.Fedoseev <Yri95@inbox.ru>
2023-05-29 18:50:44 +0300

    Master clean

--- src/test/java/guru/qa/TestOpenFileJackson.java
+++ src/test/java/guru/qa/TestOpenFileJackson.java
@@ -10,11 +10,8 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 
 public class TestOpenFileJackson {
-    //    Class cl;
-//    @BeforeAll
-//    static void beforeAll() {
+
     ObjectMapper objectMapper = new ObjectMapper().findAndRegisterModules();
-//    cl = getClass().getClassLoader();}
 
     @JsonIgnoreProperties(ignoreUnknown = true)
 
@@ -25,24 +22,8 @@ public class TestOpenFileJackson {
         File file = new File("src/test/resources/filesForLoading/lessonsList.json");
         InputStreamReader inputStreamReader = new InputStreamReader(resourceAsStream);
         JsonText jsonText = objectMapper.readValue(file, JsonText.class);
-        //(file, jsonText.getSendID());
         System.out.println(jsonText);
 
-
-//        System.out.println(objectMapper);
-//         (InputStream is = cl.getResourceAsStream("lessonsList.json");
-//             InputStreamReader jsonRead = new InputStreamReader(is))
-//         {
-//             JsonText jsonText = objectMapper.readValue(jsonRead, JsonText.class);
-//            Assertions.assertEquals("12", jsonText.getStudentId()));
-//        }
-        //       File file = new File("filesForLoading/lessonsList.json");
-        //      ParserLibrary parserLibrary = mapper.readValue(isr, ParserLibrary.class);
-        //      String json = objectMapper.writeValueAsString(file);
-        //       System.out.println(json);
-
-        //       String nameJson = objectMapper.writeValueAsString(file.getName());
-        //       System.out.println(nameJson);
     }
 
 }
