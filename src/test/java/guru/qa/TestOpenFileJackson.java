d531845c3f682aa4ab9c8dfe7901a7cce9fc44bb
Yury.Fedoseev <Yri95@inbox.ru>
2023-06-19 22:36:51 +0300

    Json 2

--- src/test/java/guru/qa/TestOpenFileJackson.java
+++ src/test/java/guru/qa/TestOpenFileJackson.java
@@ -5,28 +5,24 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 
 public class TestOpenFileJackson {
 
-    ObjectMapper objectMapper = new ObjectMapper().findAndRegisterModules();
-
     @JsonIgnoreProperties(ignoreUnknown = true)
 
     ClassLoader cl = TestOpenFileJackson.class.getClassLoader();
+
     @Test
     public void jsonOpen() throws IOException {
         ObjectMapper objectMapper = new ObjectMapper();
         try (InputStream is = cl.getResourceAsStream("filesForLoading/lessonsList.json");
              InputStreamReader isr = new InputStreamReader(is)) {
             JsonText text = objectMapper.readValue(isr, JsonText.class);
-
             Assertions.assertEquals(2, text.getStudentId());
         }
-
     }
 }
 
